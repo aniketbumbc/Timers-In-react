8c229691af1167773020d6c8167089123fdcbc34
aniket <aniket.umbc@gmail.com>
2021-07-28 07:10:57 +0530

    Timer in react

---
+++ src/components/HideText.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const styleObj = {
+  visibility: 'hidden',
+};
+
+const HideText = () => {
+  const [applyClass, setApplyClass] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setApplyClass(!applyClass);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [applyClass]);
+
+  return (
+    <>
+      {applyClass ? <h3 style={styleObj}> Text Here</h3> : <h3> Text Here</h3>}
+    </>
+  );
+};
+
+export default HideText;
